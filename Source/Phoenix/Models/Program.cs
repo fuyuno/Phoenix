c693ae29d0ac431643a54218e498039712d4b05e
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-11-11 12:37:32 +0900

    Add base classes

---
+++ Source/Phoenix/Models/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Phoenix.Models
+{
+    internal class Program
+    {
+        /// <summary>
+        ///     更新プログラム名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     DL ページ URL
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
---
+++ Source/Phoenix/Models/Vaio/Product.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Phoenix.Models.Vaio
+{
+    internal abstract class Product
+    {
+        /// <summary>
+        ///     製品名
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        ///     型番
+        /// </summary>
+        public abstract string ModelNumber { get; }
+
+        /// <summary>
+        ///     サポートページ
+        /// </summary>
+        public abstract string FeedUrl { get; }
+
+        public abstract Task<IEnumerable<Program>> Parse();
+
+        protected async Task<string> Get()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(FeedUrl);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
