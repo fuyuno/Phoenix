c023e091e2032e9978c0f2becd6cbd72d1e04820
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-11-13 06:01:04 +0900

    Z12A* シリーズの統合

---
+++ Source/Phoenix/Models/Vaio/Z12A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Phoenix.Models.Vaio
+{
+    /// <summary>
+    ///     VJZ12A
+    /// </summary>
+    internal class Z12A : Product
+    {
+        private readonly string _windows;
+        public override string Name => $"VAIO Z Canvas (Windows {_windows})";
+        public override string ModelNumber => "VJZ12A*";
+        public override string FeedUrl => "https://support.vaio.com/products/z-vjz12A/update.html";
+        public override string XPath => $"//dl[@class='information information-support'][{(_windows == "10" ? 1 : 2)}]";
+
+        public Z12A(string windows)
+        {
+            if ((windows != "10") && (windows != "8.1"))
+                throw new NotSupportedException("Supported 8.1 or 10 only.");
+            _windows = windows;
+        }
+    }
+}
