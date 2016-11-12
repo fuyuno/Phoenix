08c797b40b80f6e63208e9b5c5c4c14b5fb2359b
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-11-12 15:21:10 +0900

    Add "VAIO Z" series.

---
+++ Source/Phoenix/Models/Vaio/Z131.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Phoenix.Models.Vaio
+{
+    internal class Z131 : Product
+    {
+        public override string Name => "VAIO Z クラムシェルモデル";
+        public override string ModelNumber => "VJZ131*";
+        public override string FeedUrl => "https://support.vaio.com/products/z-vjz131/update.html";
+
+        public override Task<IEnumerable<Program>> Parse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Phoenix/Models/Vaio/Z13B.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Phoenix.Models.Vaio
+{
+    internal class Z13B : Product
+    {
+        public override string Name => "VAIO Z フリップモデル";
+        public override string ModelNumber => "VJZ13B*";
+        public override string FeedUrl => "https://support.vaio.com/products/z-vjz131/update.html";
+
+        public override Task<IEnumerable<Program>> Parse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
