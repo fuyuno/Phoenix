5be260112c32effe26726b832408653d750f5567
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-11-13 16:11:46 +0900

    VAIO Fit 15E | mk2 (VJF152*) 対応

---
+++ Source/Phoenix/Models/Vaio/F152.cs
@@ -0,0 +1,18 @@
+﻿namespace Phoenix.Models.Vaio
+{
+    /// <summary>
+    ///     VAIO Fit 15E | mk2
+    /// </summary>
+    internal class F152 : Product
+    {
+        public override string Name => $"VAIO Fit 15E | mk2 ({Windows.ToWinStr()})";
+        public override string ModelNumber => "VJF152*";
+        public override string FeedUrl => "https://support.vaio.com/products/fit-vjf152/update.html";
+        public override string XPath => $"//dl[@class='information information-support'][{Windows.ToWinIndex(1, 2, 3)}]";
+
+        public F152(Windows windows)
+        {
+            Windows = windows;
+        }
+    }
+}
