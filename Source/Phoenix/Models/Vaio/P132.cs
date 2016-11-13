30e94586ee277a23a5f38f0d8c8a7cfe94b94d92
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-11-13 16:09:54 +0900

    VAIO Pro 13 | mk2 (VJP132*) 対応

---
+++ Source/Phoenix/Models/Vaio/P132.cs
@@ -0,0 +1,18 @@
+﻿namespace Phoenix.Models.Vaio
+{
+    /// <summary>
+    ///     VAIO Pro 13 | mk2
+    /// </summary>
+    internal class P132 : Product
+    {
+        public override string Name => $"VAIO Pro 13 | mk2 ({Windows.ToWinStr()})";
+        public override string ModelNumber => "VJP132*";
+        public override string FeedUrl => "https://support.vaio.com/products/pro-vjp132/update.html";
+        public override string XPath => $"//dl[@class='information information-support'][{Windows.ToWinIndex(1, 2, 3)}]";
+
+        public P132(Windows windows)
+        {
+            Windows = windows;
+        }
+    }
+}
