2825e20620edd984aa0131d0dd64c9c1c62830ce
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-11-13 16:08:02 +0900

    VAIO C15 (VJC151*) 対応

---
+++ Source/Phoenix/Models/Vaio/C151.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Phoenix.Models.Vaio
+{
+    /// <summary>
+    ///     VAIO C15
+    /// </summary>
+    internal class C151 : Product
+    {
+        public override string Name => $"VAIO C15 ({Windows.ToWinStr()})";
+        public override string ModelNumber => "VJC151*";
+        public override string FeedUrl => "https://support.vaio.com/products/c15/update.html";
+        public override string XPath => $"//dl[@class='information information-support'][{Windows.ToWinIndex()}]";
+
+        public C151(Windows windows)
+        {
+            if (windows == Windows.Windows81)
+                throw new NotSupportedException();
+            Windows = windows;
+        }
+    }
+}
