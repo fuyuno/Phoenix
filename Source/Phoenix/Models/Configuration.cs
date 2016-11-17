157cbc78b155f53455d454d690b85e56cbcf18f8
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-11-17 10:28:23 +0900

    最終チェック日時追加

--- Source/Phoenix/Models/Configuration.cs
+++ Source/Phoenix/Models/Configuration.cs
@@ -1,8 +1,12 @@
-﻿namespace Phoenix.Models
+﻿using System;
+
+namespace Phoenix.Models
 {
     internal class Configuration
     {
         public Interval Interval { get; set; } = Interval.Daily;
+
+        public DateTime CheckedAt { get; set; } = DateTime.MinValue;
 
         public Windows Windows { get; set; } = SystemInformation.GetOsVersion();
 
